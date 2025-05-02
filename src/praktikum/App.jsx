ef4cfc29e74b0638654a3218cd3b2267173fee1d
Yura <yuhaya.lissera@gmail.com>
2025-05-02 19:37:04 +0700

    refactor: Clean up imports and organize lazy-loaded components in App.jsx

--- src/praktikum/App.jsx
+++ src/praktikum/App.jsx
@@ -1,15 +1,21 @@
 import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./assets/tailwind.css";
-import NotFound from "./pages/NotFound";
-import ErrorPage403 from "./pages/ErrorPage403";
-import ErrorPage401 from "./pages/ErrorPage401";
-import ErrorPage400 from "./pages/ErrorPage400";
-import FormCustomer from "./pages/FormCustomer";
+// import NotFound from "./pages/NotFound";
+// import ErrorPage403 from "./pages/ErrorPage403";
+// import ErrorPage401 from "./pages/ErrorPage401";
+// import ErrorPage400 from "./pages/ErrorPage400";
+
 
 import Loading from "./components/Loading";
-import UserList from "./pages/UserList";
+// import UserList from "./pages/UserList";
 
+const NotFound = React.lazy(() => import("./pages/NotFound"));
+const ErrorPage403 = React.lazy(() => import("./pages/ErrorPage403"));
+const ErrorPage401 = React.lazy(() => import("./pages/ErrorPage401"));
+const ErrorPage400 = React.lazy(() => import("./pages/ErrorPage400"));
+const UserList = React.lazy(() => import("./pages/UserList"));
+const FormCustomer = React.lazy(() => import("./pages/FormCustomer"));
 const FormOrder = React.lazy(() => import("./pages/FormOrder"));
 const MainLayout = React.lazy(() => import("./layouts/MainLayout"));
 const AuthLayout = React.lazy(() => import("./layouts/AuthLayout"));
@@ -19,6 +25,7 @@ const Forgot = React.lazy(() => import("./pages/auth/Forgot"));
 const Orders = React.lazy(() => import("./pages/Order"));
 const Customer = React.lazy(() => import("./pages/Customer"));
 const Dashboard = React.lazy(() => import("./pages/Dashboard"));
+
 
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
@@ -42,7 +49,7 @@ function App() {
       <Routes>
         <Route element={<MainLayout />}>
           <Route path="*" element={<NotFound />} />
-          <Route path="/" element={<Dashboard />} />
+          <Route path="/praktikum" element={<Dashboard />} />
           <Route path="/order" element={<Orders />} />
           <Route path="/customer" element={<Customer />} />
           <Route path="/FormCustomer" element={<FormCustomer />} />
